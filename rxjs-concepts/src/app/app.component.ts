29a9ffd28517e111925a3ac7438da72d3f13b9a2
Luis Fullone DECSKILL <luishenriquevieirafullone.decskill@edp.com>
2024-11-06 17:43:15 +0000

    merge map example

--- rxjs-concepts/src/app/app.component.ts
+++ rxjs-concepts/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ConcatMapComponent } from './concat-map/concat-map.component';
+import { MergeMapComponent } from './merge-map/merge-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ConcatMapComponent],
+  imports: [ConcatMapComponent, MergeMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- rxjs-concepts/src/app/merge-map/merge-map.component.ts
+++ rxjs-concepts/src/app/merge-map/merge-map.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BasicFormDirective } from '../directives/basic-form.directive';
+import { delay, mergeMap, takeUntil } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-merge-map',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './merge-map.component.html',
   styleUrl: './merge-map.component.scss'
 })
-export class MergeMapComponent {
+export class MergeMapComponent extends BasicFormDirective implements OnInit {
 
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(
+      takeUntil(this.destroyed),
+      mergeMap(formValue => this.formHistoryService.saveHistory(formValue).pipe(delay((Math.random()*1000))))
+    ).subscribe(
+      {
+        next: (savedValue) => { console.log('Handle with saved value', savedValue) },
+        error: (error) => { console.log('Error handler', error) },
+     }
+    );
+  }
 }
