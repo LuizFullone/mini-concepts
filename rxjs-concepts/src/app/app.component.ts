baeb4c8a0ecb2d0e23aa7b30100457742a38e902
Luis Fullone DECSKILL <luishenriquevieirafullone.decskill@edp.com>
2024-11-06 18:05:42 +0000

    switch map example

--- rxjs-concepts/src/app/app.component.ts
+++ rxjs-concepts/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ConcatMapComponent } from './concat-map/concat-map.component';
 import { MergeMapComponent } from './merge-map/merge-map.component';
+import { SwitchMapComponent } from './switch-map/switch-map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ConcatMapComponent, MergeMapComponent],
+  imports: [ConcatMapComponent, MergeMapComponent, SwitchMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- rxjs-concepts/src/app/switch-map/switch-map.component.ts
+++ rxjs-concepts/src/app/switch-map/switch-map.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BasicFormDirective } from '../directives/basic-form.directive';
+import { delay, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-switch-map',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './switch-map.component.html',
   styleUrl: './switch-map.component.scss'
 })
-export class SwitchMapComponent {
+export class SwitchMapComponent extends BasicFormDirective implements OnInit {
 
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(
+      takeUntil(this.destroyed),
+      switchMap(formValue => this.formHistoryService.saveHistory(formValue).pipe(delay((400))))
+    ).subscribe(
+      {
+        next: (savedValue) => { console.log('Handle with saved value', savedValue) },
+        error: (error) => { console.log('Error handler', error) },
+     }
+    );
+  }
 }
